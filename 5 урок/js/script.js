70bbc513f9cb4b60b4cb26510efd602814a1492e
4elus <m_alex02@mail.ru>
2020-03-22 15:31:54 +0400

    CSS3 Gradient and JS

---
+++ 5 урок/js/script.js
@@ -0,0 +1,20 @@
+//var name = prompt("Enter name: ")
+//var surname = prompt("Enter surname: ");
+
+var isStudent = true;
+
+
+/*if (name == "Alex" || surname == "Mayhem"){
+	alert("Hello, " +  name);
+}
+else{
+	alert("I don't know you");
+}*/
+
+if (!isStudent){
+	alert("You student");
+}
+else{
+	alert("You don't student");
+}
+
