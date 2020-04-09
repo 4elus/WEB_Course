325ac221f8efa081fd2a6fc76ef6bac236564f0e
4elus <m_alex02@mail.ru>
2020-04-09 18:41:12 +0400

    Game reaction JS

---
+++ 11 урок/js/script.js
@@ -0,0 +1,49 @@
+var startTime = new Date().getTime(); // 00:00
+
+
+var makeShapeVisible = function() {
+	 document.getElementById("shape").style.display = 'block';
+	 document.getElementById("shape").style.width = getRandonInt(150) + 50 + "px"
+	 document.getElementById("shape").style.height = getRandonInt(150) + 50 + "px"
+	 document.getElementById("shape").style.left = getRandonInt(1000) + 50 + "px"
+	 document.getElementById("shape").style.top = getRandonInt(500) + 50 + "px"
+
+	 if (Math.random() < 0.25){
+	 	document.getElementById("shape").style.borderRadius = "0";
+	 	document.getElementById("shape").style.borderBottom = '0';
+	 	document.getElementById("shape").style.backgroundColor = 'orange';
+	 } else if (Math.random() >= 0.25 && Math.random() <= 0.60){
+	 	 document.getElementById("shape").style.borderRadius = getRandonInt(80) + 20 + "%";
+	 	 document.getElementById("shape").style.borderBottom = '0';
+	 	 document.getElementById("shape").style.backgroundColor = 'orange';
+	 } else{
+	 	 document.getElementById("shape").style.width = "0";
+	 	 document.getElementById("shape").style.height = "0";
+	 	 document.getElementById("shape").style.borderRadius = "0";
+	 	 document.getElementById("shape").style.borderLeft = "50px solid transparent";
+	 	 document.getElementById("shape").style.borderRight = "50px solid transparent";
+	 	 document.getElementById("shape").style.borderBottom = '100px solid orange';
+	 	 document.getElementById("shape").style.backgroundColor = 'transparent';
+	 }
+
+	 startTime = new Date().getTime();
+}
+
+makeShapeVisible();
+
+document.getElementById("shape").addEventListener("click", function() {
+	var finishTime = new Date().getTime();
+	var reactionTime = (finishTime - startTime) / 1000;
+	document.getElementById("time").innerHTML = "Time: " + reactionTime; 
+	setTimeout(makeShapeVisible(), Math.random() * 1500);
+});
+
+
+function getRandonInt (max) {
+	return Math.random() * max; 
+}
+
+function getRandomColor() {
+	// Your code here...
+	
+}
