3e5a7f23828ae1d49961609eec180fbfc8d8f301
4elus <m_alex02@mail.ru>
2020-04-12 15:40:03 +0400

    jQuery

--- 11 урок/js/script.js
+++ 11 урок/js/script.js
@@ -1,49 +1,102 @@
 var startTime = new Date().getTime(); // 00:00
 
 
+
+
+
 var makeShapeVisible = function() {
+
 	 document.getElementById("shape").style.display = 'block';
+
 	 document.getElementById("shape").style.width = getRandonInt(150) + 50 + "px"
+
 	 document.getElementById("shape").style.height = getRandonInt(150) + 50 + "px"
+
 	 document.getElementById("shape").style.left = getRandonInt(1000) + 50 + "px"
+
 	 document.getElementById("shape").style.top = getRandonInt(500) + 50 + "px"
 
+
+
 	 if (Math.random() < 0.25){
+
 	 	document.getElementById("shape").style.borderRadius = "0";
+
 	 	document.getElementById("shape").style.borderBottom = '0';
-	 	document.getElementById("shape").style.backgroundColor = 'orange';
+
+	 	document.getElementById("shape").style.backgroundColor = getRandomColor();
 	 } else if (Math.random() >= 0.25 && Math.random() <= 0.60){
+
 	 	 document.getElementById("shape").style.borderRadius = getRandonInt(80) + 20 + "%";
+
 	 	 document.getElementById("shape").style.borderBottom = '0';
-	 	 document.getElementById("shape").style.backgroundColor = 'orange';
+
+	 	 document.getElementById("shape").style.backgroundColor = getRandomColor();
+
 	 } else{
+
 	 	 document.getElementById("shape").style.width = "0";
+
 	 	 document.getElementById("shape").style.height = "0";
+
 	 	 document.getElementById("shape").style.borderRadius = "0";
+
 	 	 document.getElementById("shape").style.borderLeft = "50px solid transparent";
+
 	 	 document.getElementById("shape").style.borderRight = "50px solid transparent";
-	 	 document.getElementById("shape").style.borderBottom = '100px solid orange';
+
+	 	 document.getElementById("shape").style.borderBottom = '100px solid ' + getRandomColor();
+
 	 	 document.getElementById("shape").style.backgroundColor = 'transparent';
+
 	 }
 
+
+
 	 startTime = new Date().getTime();
+
 }
+
+
 
 makeShapeVisible();
 
+
+
 document.getElementById("shape").addEventListener("click", function() {
+
 	var finishTime = new Date().getTime();
+
 	var reactionTime = (finishTime - startTime) / 1000;
+
 	document.getElementById("time").innerHTML = "Time: " + reactionTime; 
+
 	setTimeout(makeShapeVisible(), Math.random() * 1500);
+
 });
 
 
+
+
+
 function getRandonInt (max) {
+
 	return Math.random() * max; 
+
 }
 
+
 function getRandomColor() {
-	// Your code here...
-	
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+
+
+function setRandomColor() {
+  $("#colorpad").css("background-color", getRandomColor());
 }
---
+++ 12 урок/js/js.js
@@ -0,0 +1,48 @@
+/*if (jQuery){
+	alert("Hi jQuery");
+}*/
+
+$(document).ready(function(){
+	/*
+		CSS
+	var myStyle = {
+		color: 'purple',
+		background: 'orange',
+		border: '5px solid red'
+	};
+
+	$('h2').css(myStyle);
+
+	$('li').css("color", "orange");*/
+
+/* TEXT()
+	console.log($('ol li').text());
+	$('h2').text('New value');*/
+
+		// HTML()
+	//console.log($('li').html('<a href="http://google.com">Google</a>'));
+
+/* attr()
+	$('#photo').click(function(){
+		$(this).attr({
+			src: "img/orange.jpg",
+			width: "150px",
+			height: '150px'
+		})
+	});*/
+
+//VAL()
+//	$('input').val("Alexander");
+
+// CLASS()
+/*
+	//$('li:gt(2)').addClass('first');
+	//$('li').removeClass('first');
+	$("li").toggleClass('first');
+	$("li").toggleClass('first');*/
+
+	$('li').click(function(){
+		$(this).toggleClass("last");
+	});
+});
+
